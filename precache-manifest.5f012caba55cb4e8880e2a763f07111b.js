d3c11b56614c93ae3a3a435778bef6b2b8ca0238
Alice <carrizoalice@gmail.com>
2020-10-27 10:44:47 -0300

    Updates

---
+++ precache-manifest.5f012caba55cb4e8880e2a763f07111b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe03fc1741ac9a009d41c1f4a7cc523",
+    "url": "/floxynews/index.html"
+  },
+  {
+    "revision": "5b1a43acc4d2a9db450e",
+    "url": "/floxynews/static/css/2.e24a89a4.chunk.css"
+  },
+  {
+    "revision": "1899819dee3290e1be35",
+    "url": "/floxynews/static/css/main.655937bd.chunk.css"
+  },
+  {
+    "revision": "5b1a43acc4d2a9db450e",
+    "url": "/floxynews/static/js/2.650b9591.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/floxynews/static/js/2.650b9591.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1899819dee3290e1be35",
+    "url": "/floxynews/static/js/main.11dab2d9.chunk.js"
+  },
+  {
+    "revision": "a7d99f76fbda0a02f2d6",
+    "url": "/floxynews/static/js/runtime-main.fc16e702.js"
+  }
+]);
